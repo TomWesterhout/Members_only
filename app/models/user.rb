784a507bdf720941686bcf2a20bd529570f475da
TomWesterhout <westerhout_tom@hotmail.com>
2017-07-23 16:51:31 +0200

    Created login form and corresponding actions/methods in the sessions controller and sessionshelper for the login process.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,21 @@
 class User < ApplicationRecord
+	attr_accessor :remember_token
 	has_secure_password
+
+	def self.new_token
+		SecureRandom.urlsafe_base64
+	end
+
+	def self.digest(string)
+		Digest::SHA1.hexdigest(string)
+	end
+
+	def remember
+		self.remember_token = User.new_token
+		update_attribute(:remember_digest, User.digest(remember_token))
+	end
+
+	def forget
+		update_attribute(:remember_digest, nil)
+	end
 end
