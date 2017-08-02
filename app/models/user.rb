fedd9606c4cf262ae9c21f2f9d1432bc647345dd
TomWesterhout <westerhout_tom@hotmail.com>
2017-08-02 17:23:24 +0200

    Added validations to user and post models.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User < ApplicationRecord
 	has_secure_password
 	has_many :posts
 
+	validates :name, presence: true, length: { maximum: 40 }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+	validates :email, presence: true, length: { maximum: 255 },
+	format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+	validates :password, presence: true, length: { minimum: 6 }, allow_nil: false
+
 	def self.new_token
 		SecureRandom.urlsafe_base64
 	end
